d5a26508625ad7d8926606e839f782d25721ea97
Gauthier Queau <queau.gauthier@orange.fr>
2019-12-13 11:53:38 +0100

    splashScreen2

--- app/src/main/java/com/example/mydigitalproject/SplashScreen.java
+++ app/src/main/java/com/example/mydigitalproject/SplashScreen.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
@@ -12,7 +14,7 @@ import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 4000;
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
 
     @Override
@@ -20,7 +22,14 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        ImageView logo = (ImageView) findViewById(R.id.logo);
 
+        Animation fadeOut = new AlphaAnimation(1, 0);  // the 1, 0 here notifies that we want the opacity to go from opaque (1) to transparent (0)
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500); // Start fading out after 500 milli seconds
+        fadeOut.setDuration(1000); // Fadeout duration should be 1000 milli seconds
+
+        logo.setAnimation(fadeOut);
 
 
         /* New Handler to start the Menu-Activity
@@ -31,7 +40,6 @@ public class SplashScreen extends AppCompatActivity {
                 /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
                 SplashScreen.this.startActivity(mainIntent);
-                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
                 SplashScreen.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
